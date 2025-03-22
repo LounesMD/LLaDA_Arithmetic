cac2dd231061f0ee5d1c165ed392b36d160b1315
Tom <tom.ravaud@eleves.enpc.fr>
2025-03-22 16:13:34 +0100

    Add the naive tokenizer

--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -1,7 +1,41 @@
 import re
 
 
-class character_level_tokenizer:
+class naive_tokenizer:
+    """
+    character-level
+    """
+    def __init__(
+        self, number_bits, pad_token="[PAD]", eos_token="[EOS]", mask_token="[MASK]"
+    ):
+        self.vocab = [str(x) for x in range(10)] + ["+", "="] + [pad_token, eos_token, mask_token]
+        self.token_to_id = {v : k for k, v in enumerate(self.vocab)}
+        self.id_to_token = {k : v for k, v in enumerate(self.vocab)}
+        self.ntokens = len(self.vocab)
+        self.pattern = f"[^{re.escape(''.join(self.vocab))}]"
+
+    def clean(self, text):
+        """
+        removes all characters not in the vocabulary
+        """
+        out = re.sub(self.pattern, "", text)
+        return out
+
+    def pre_tokenization(self, text):
+        """
+        character-level
+        """
+        return [c for c in text]
+
+    def encode(self, text):
+        text_list = self.pre_tokenization(self.clean(text))
+        return [self.token_to_id[c] for c in text_list]
+
+    def decode(self, token_list):
+        return "".join([self.id_to_token[x] for x in token_list])
+
+
+class naive_pad_tokenizer:
     """
     character-level
     """
@@ -88,11 +122,10 @@ class character_level_tokenizer:
         )
 
 
-class character_level_tokenizer2:
+class group_pad_tokenizer:
     """
     character-level
     """
-
     def __init__(
         self, number_bits, pad_token="[PAD]", eos_token="[EOS]", mask_token="[MASK]"
     ):
